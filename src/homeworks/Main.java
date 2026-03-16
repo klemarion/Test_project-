44d84910158cf343f75a2883f3248534f64725cb
klemarion <szaharcenko204@gmail.com>
2026-03-16 21:42:20 +0100

    add code

---
+++ src/homeworks/Main.java
@@ -0,0 +1,25 @@
+package homeworks;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+            String day = "Monday";
+
+            switch (day) {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    System.out.println("Робочий день");
+                    break;
+
+                case "Saturday":
+                case "Sunday":
+                    System.out.println("Вихідний день");
+                    break;
+            }
+        }
+    }
+}

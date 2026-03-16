0a8a0f59e3447f13458f011f27136c06482510f6
klemarion <szaharcenko204@gmail.com>
2026-03-16 20:30:39 +0100

    add code line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
+        public static int linearSearch(int[] arr, int target) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == target) {
+                    return i;
+                }
+            }
+            return -1;
         }
 
     }

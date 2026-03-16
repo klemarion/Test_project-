4c1f0b7b03c5c4dcb4dbc08633a6c0625525fb58
klemarion <szaharcenko204@gmail.com>
2026-03-16 21:50:04 +0100

    add code

--- src/homeworks/Main.java
+++ src/homeworks/Main.java
@@ -23,3 +23,23 @@ public class Main {
         }
     }
 }
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 7, 9};
+        int result = linearSearch(arr, 7);
+
+        System.out.println(result);
+    }
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}

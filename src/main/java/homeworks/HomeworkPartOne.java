263b25def99e9707cd20dc4d22d54aad55330be5
Anna Piontkivska <anna.piontkivska@privatbank.ua>
2026-03-23 13:51:09 +0200

    updated class name

--- src/main/java/homeworks/HomeworkPartOne.java
+++ src/main/java/homeworks/HomeworkPartOne.java
@@ -2,7 +2,7 @@ package homeworks;
 import java.util.Scanner;
 
 
-public class homework_01_age {
+public class HomeworkPartOne {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
 

b70cd3ec48f30b0d3fe5f4b4aba5abc1a02d80d4
klemarion <szaharcenko204@gmail.com>
2026-05-20 14:01:48 +0300

    create api_tests

---
+++ src/test/java/homeworks/api/Pet.java
@@ -0,0 +1,11 @@
+package homeworks.api;
+
+import java.util.List;
+
+public class Pet {
+    public long id;
+    public String name;
+    public List<String> photoUrls;
+    public List<String> tags;
+    public String status;
+}
